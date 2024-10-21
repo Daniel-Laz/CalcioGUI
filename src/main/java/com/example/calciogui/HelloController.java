807bfccfd7fed5f944baf22584a4912f72ad8977
Lazzaroni.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-21 13:47:55 +0200

    primi due metodi

--- src/main/java/com/example/calciogui/HelloController.java
+++ src/main/java/com/example/calciogui/HelloController.java
@@ -3,7 +3,7 @@ package com.example.calciogui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import calcio.Calciatore;
 import calcio.Squadra;
@@ -11,7 +11,13 @@ import calcio.Squadra;
 public class HelloController {
     private Squadra squadra= new Squadra();
     @FXML
-    private TextField input;
+    private TextField nome;
+
+    @FXML
+    private TextField goal;
+    @FXML
+    private CheckBox capitano;
+
     @FXML
     private Label output;
     @FXML
@@ -26,11 +32,15 @@ public class HelloController {
 
     @FXML
     protected void first() {
-        output.setText("inserisci in ordine: nome INVIO goal INVIO capitano? (true o false)");
-        if (squadra.addCalciatore(input.getText(), Integer.parseInt(input.getText()), Boolean.parseBoolean(input.getText()))) {
+
+        if (squadra.addCalciatore(nome.getText(), Integer.parseInt(goal.getText()), capitano.isSelected())) {
             output.setText("giocatore aggiunto");
         } else {
             output.setText("giocatore non aggiunto - è già presente un capitano");
         }
+    }
+    @FXML
+    protected String enter() {
+        return output.getText();
     }
 }
