818787603482c3cbcdcee438c4038b505f1ff06c
Lazzaroni.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-28 12:17:42 +0100

    quarto quinto sesto metodo

--- src/main/java/com/example/calciogui/HelloController.java
+++ src/main/java/com/example/calciogui/HelloController.java
@@ -12,12 +12,10 @@ public class HelloController {
     private Squadra squadra= new Squadra();
     @FXML
     private TextField nome;
-
     @FXML
     private TextField goal;
     @FXML
     private CheckBox capitano;
-
     @FXML
     private Label output;
     @FXML
@@ -26,7 +24,21 @@ public class HelloController {
     private Button first;
     @FXML
     private Button third;
+    @FXML
+    private Button fourth;
+    @FXML
+    private Button fifth;
+    @FXML
+    private Button sixth;
 
+    @FXML
+    protected void first() {
+        if (squadra.addCalciatore(nome.getText(), Integer.parseInt(goal.getText()), capitano.isSelected())) {
+            output.setText("giocatore aggiunto");
+        } else {
+            output.setText("giocatore non aggiunto - è già presente un capitano");
+        }
+    }
 
     @FXML
     protected void second() {
@@ -34,23 +46,31 @@ public class HelloController {
     }
 
     @FXML
-    protected void first() {
-
-        if (squadra.addCalciatore(nome.getText(), Integer.parseInt(goal.getText()), capitano.isSelected())) {
-            output.setText("giocatore aggiunto");
-        } else {
-            output.setText("giocatore non aggiunto - è già presente un capitano");
-        }
-    }
-    @FXML
     protected void third() {
-        output.setText(squadra.printSquadra());
-
         if (squadra.editGiocatore(nome.getText(), nome.getText(), Integer.parseInt(goal.getText()), capitano.isSelected())) {
             output.setText("giocatore modificato");
         } else {
             output.setText("giocatore non presente");
         }
+    }
+
+    @FXML
+    protected void fourth() {
+        if (squadra.delGiocatore(nome.getText())){
+            output.setText("giocatore eliminato");
+        } else {
+            output.setText("giocatore non trovato");
+        }
+    }
+
+    @FXML
+    protected void fifth() {
+        output.setText(squadra.goal5());
+    }
+
+    @FXML
+    protected void sixth() {
+        output.setText(squadra.getCapitano());
     }
 
 }
